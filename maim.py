1618023e417309ac76504c85744ec537fde18fed
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2024-12-22 13:40:00 +0200

    INTRODUCTION TO PYTHON

---
+++ maim.py
@@ -0,0 +1,7 @@
+print("My name is Vivian Nwasinachi")
+print("I am 14 years old.")
+print("I am in Grade 10.\nI love reading and coding")
+print("")
+import keyword
+print(keyword.kwlist)
+print(True)
